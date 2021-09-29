1fb95afa5b97894961ca791a5efb41f6153d4d1b
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-09-29 08:25:28 +0200

    Short section names
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- list.h
+++ list.h
@@ -2,7 +2,7 @@
 
 /*
  * In the linker script this is the relevant line:
- * .opensbilist	: { KEEP(*(SORT(opensbilist*))); }
+ * .sbilist	: { KEEP(*(SORT(sbilist*))); }
  */
 
 #ifndef _LIST_H
@@ -14,29 +14,29 @@
 
 #define list_entry(type, list, item) \
 	type \
-	__attribute__((section("opensbilist_"#list"_2_"#item))) \
+	__attribute__((section(".sbilist_"#list"_2_"#item))) \
 	__attribute__((unused)) \
 	__attribute__((__aligned__(__alignof__(type)))) \
-	opensbilist_##list##_0_##item
+	sbilist_##list##_0_##item
 
 #define list_start(type, list) \
 	type \
-	__attribute__((section("opensbilist_"#list"_1_"))) \
+	__attribute__((section(".sbilist_"#list"_1_"))) \
 	__attribute__((unused)) \
 	__attribute__((__aligned__(__alignof__(type)))) \
-	opensbilist_##list##_0_##start[0]
+	sbilist_##list##_0_##start[0]
 
 #define list_end(type, list) \
 	type \
-	__attribute__((section("opensbilist_"#list"_3_"))) \
+	__attribute__((section(".sbilist_"#list"_3_"))) \
 	__attribute__((unused)) \
 	__attribute__((__aligned__(1))) \
-	opensbilist_##list##_0_##end[0]
+	sbilist_##list##_0_##end[0]
 
 #define list_get_start(type, list) \
-	(opensbilist_##list##_0_##start)
+	(sbilist_##list##_0_##start)
 
 #define list_get_end(type, list) \
-	(opensbilist_##list##_0_##end)
+	(sbilist_##list##_0_##end)
 
 #endif
